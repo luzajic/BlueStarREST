3b1bc5ebc2455717a205a5ceec0f0e5ab53fd4ac
Vladan Luzajic <Vladan.Luzajic@StadaITSolutions.com>
2019-05-07 13:28:29 +0200

    Initial commit

---
+++ BlueStarREST/Controllers/UsersController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BlueStarREST.Models;
+
+namespace BlueStarREST.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private RucakBazaEntities db = new RucakBazaEntities();
+
+        // GET: api/Users
+        public IQueryable<Users> GetUsers()
+        {
+            return db.Users;
+        }
+
+        //public IHttpActionResult Login(string username, string password)
+        //{
+        //    try
+        //    {
+        //        Users u = db.Users.Where(x => x.Username == username).FirstOrDefault();
+
+        //    }
+        //    catch
+        //    {
+
+        //    }
+            
+        //}
+
+        // GET: api/Users/5
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult GetUsers(int id)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(users);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUsers(int id, Users users)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != users.UserId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult PostUsers([FromBody] Users users)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                db.Users.Add(users);
+                db.SaveChanges();
+
+                return CreatedAtRoute("DefaultApi", new { id = users.UserId }, users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult DeleteUsers(int id)
+        {
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(users);
+            db.SaveChanges();
+
+            return Ok(users);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsersExists(int id)
+        {
+            return db.Users.Count(e => e.UserId == id) > 0;
+        }
+    }
+}
